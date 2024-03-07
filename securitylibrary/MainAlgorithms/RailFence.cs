dff14542a73584a6d35144a7629d44c79759dea5
Filobateer Essam <108236186+FilobateerEssam@users.noreply.github.com>
2024-03-06 20:01:21 -0800

    Finished RailFence

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,18 +10,96 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            plainText = plainText.ToLower();
+            string pt = "";
+            int key = 1;
+            while (key != plainText.Length)
+            {
+                pt = Decrypt(cipherText, key);
+                if (pt.Length > plainText.Length)
+                {
+                    pt = pt.Remove(plainText.Length);
+                }
+                if (pt.ToString().Equals(plainText.ToString()))
+                {
+                    return key;
+                }
+                else
+                {
+                    key++;
+                }
+            }
+            return 0;
 
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            int row = key;
+            string pl = "";
+            cipherText = cipherText.ToLower();
+            int column = (int)Math.Ceiling((double)cipherText.Length / (double)key);
+
+            char[,] arr = new char[row, column];
+            int counter = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (counter < cipherText.Length)
+                    {
+                        arr[i, j] = cipherText[counter];
+                        counter++;
+                    }
+
+                }
+            }
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    pl += arr[j, i];
+
+                }
+
+            }
+            return pl;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            int row = key;
+            int column = (plainText.Length / key) + 1;
+            char[,] arr = new char[row, column];
+            char[,] res = new char[row, column];
+            string CT = "";
+            plainText = plainText.ToUpper();
+            int counter = 0;
+            for (int i = 0; i < column; i++)//3
+            {
+                for (int j = 0; j < row; j++)//
+                {
+                    if (counter < plainText.Length)
+                    {
+                        arr[j, i] = plainText[counter];
+                        counter++;
+                    }
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    CT += arr[i, j];
+                }
+            }
+            return CT;
         }
     }
 }
