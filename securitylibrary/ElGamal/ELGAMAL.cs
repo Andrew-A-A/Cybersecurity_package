89cd1d3aa7bb3b2fa974a0a80e22c36709c995f0
Mehrael <mehraelashraf20@gmail.com>
2024-05-06 18:40:32 +0300

    ElGamal Algorithm Done

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SecurityLibrary.AES;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,16 +21,38 @@ namespace SecurityLibrary.ElGamal
 
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
+            List<long> Cipher = new List<long>();
 
-            throw new NotImplementedException();
+            long C1 = Calc(alpha, k, q);
+            Cipher.Add(C1);
 
+            long K = Calc(y, k, q);
+            long C2 = (K * m) % q;
+            Cipher.Add(C2);
+
+            return Cipher;
         }
      
         public int Decrypt(int c1, int c2, int x, int q)
         {
+            int y = (int)Calc(c1, x, q);
 
-            throw new NotImplementedException();
+            AES.ExtendedEuclid EE = new AES.ExtendedEuclid();
+            int MulInv = EE.GetMultiplicativeInverse(y, q);
 
+            int M = (c2 * MulInv) % q;
+
+            return M;
+        }
+
+        private long Calc(int base_, int power, int mod)
+        {
+            long num = 1;
+
+            for (int i = 0; i < power; i++)
+                    num = (num * base_) % mod;
+
+            return num;
         }
     }
 }
