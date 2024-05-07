b2807a3baa9e778fc02b1d6e6cf4ba615216e80c
VeronicaEmad <emadveronica86@gmail.com>
2024-05-07 18:02:50 +0300

    Deffie Hellman is done

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -14,13 +14,32 @@ namespace SecurityLibrary.DiffieHellman
 
       public  int power(int f, int s, int sf)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int result = 1;
+            f = f % sf;
+
+            while (s > 0)
+            {
+                if ((s & 1) > 0)
+                    result = (result * f) % sf;
+
+                s = s >> 1;
+                f = (f * f) % sf;
+            }
+            return result;
         }
 
        public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
 
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int A = power(alpha, xa, q);
+            int B = power(alpha, xb, q);
+
+            int keyA = power(B, xa, q);
+            int keyB = power(A, xb, q);
+
+            return new List<int>() { keyA, keyB };
         }
     }
 }
